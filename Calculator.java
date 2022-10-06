a10973758c342ea5f5aafd393c6660b34b3f7fd4
Liam <reillyl8@tcd.ie>
2022-10-06 13:51:38 +0100

    Changed checkExpression method to make it work

--- Calculator.java
+++ Calculator.java
@@ -41,7 +41,7 @@ public class Calculator {
         }
         for(int i = 0;i < s.length();i++){
             char c = s.charAt(i);
-            if(Character.isDigit(i) || c != '*' || c != '+' || c!= '-')
+            if(Character.isDigit(i) && c != '*' && c != '+' && c!= '-')
                 {
                     return false;
                 }
