4c3522106c9d40809a30e399e87e9fd058bdfb57
Liam <reillyl8@tcd.ie>
2022-10-06 12:54:34 +0100

    Adding start code

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+/**
+ * Calculator
+ */
+import java.util.*;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String expression = input.nextLine();
+
+    }
+    public boolean checkString(String s){
+        for(int i = 0;i < s.length();i++){
+            char c = s.charAt(i);
+            if(Character.isDigit(c))
+        }
+        return true;
+    }
+    
+}
