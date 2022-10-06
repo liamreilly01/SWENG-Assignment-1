6931f39d93ef457cd65e9de5af4b20d625c3632a
Liam <reillyl8@tcd.ie>
2022-10-06 13:39:31 +0100

    Added a checkString method

--- Calculator.java
+++ Calculator.java
@@ -9,14 +9,29 @@ public class Calculator {
     public Calculator()
     {
         input = new Scanner(System.in);
+        if(checkString(expression)) {
+        	System.out.print("Yes");
+        }
+        else {
+        	System.out.print("No");
+        }
     }
 
-    public boolean checkString(String s){
+    public static boolean checkString(String s){
+        char start = s.charAt(0);
+        int length = s.length();
+        char end = s.charAt(length-1);
+        if(!Character.isDigit(start) || !Character.isDigit(end)){
+            return false;
+        }
         for(int i = 0;i < s.length();i++){
             char c = s.charAt(i);
-            if(Character.isDigit(c))
+            if(Character.isDigit(i) || c != '*' || c != '+' || c!= '-')
+                {
+                    return false;
+                }
+            
         }
         return true;
     }
-    
 }
