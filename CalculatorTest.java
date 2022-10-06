2593ed04fee349e2ae19073d1b1d98553daada31
RADRI <radridri@gmail.com>
2022-10-07 00:23:04 +0100

    initial commit

---
+++ CalculatorTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@SuppressWarnings("unused")
+@RunWith(JUnit4.class)
+public class CalculatorTest
+{
+	@Test
+	public void testConstructor()
+	{
+		new Calculator();
+	}
+
+}
