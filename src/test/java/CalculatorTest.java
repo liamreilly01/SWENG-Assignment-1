3a568e4fe020ce622aeab4498d0264f2ba77b0a8
RADRI <radridri@gmail.com>
2022-10-10 14:09:59 +0100

    Change names to make tests more readable

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -20,89 +20,89 @@ public class CalculatorTest
 	public void addWithOneDigitWidthTest() {
 		Calculator cal = new Calculator();
 		String expression = "1+2";
-		String answer = "3";
-		String expectedResult = cal.calculate(expression);
-		Assert.assertEquals(expectedResult, answer);
+		String expectedResult = "3";
+		String answerReceived = cal.calculate(expression);
+		Assert.assertEquals(expectedResult, answerReceived);
 	}
 	
 	@Test
 	public void subtractWithOneDigitWidthTest() {
 		Calculator cal = new Calculator();
 		String expression = "2-1";
-		String answer = "1";
-		String expectedResult = cal.calculate(expression);
-		Assert.assertEquals(expectedResult, answer);
+		String expectedResult = "1";
+		String answerReceived = cal.calculate(expression);
+		Assert.assertEquals(expectedResult, answerReceived);
 	}
 	
 	@Test
 	public void multiplyWithOneDigitWidthTest() {
 		Calculator cal = new Calculator();
 		String expression = "2*2";
-		String answer = "4";
-		String expectedResult = cal.calculate(expression);
-		Assert.assertEquals(expectedResult, answer);
+		String expectedResult = "4";
+		String answerReceived = cal.calculate(expression);
+		Assert.assertEquals(expectedResult, answerReceived);
 	}
 	
 	@Test
 	public void multiplyWithMultipleDigitWidthTest() {
 		Calculator cal = new Calculator();
 		String expression = "22*10";
-		String answer = "220";
-		String expectedResult = cal.calculate(expression);
-		Assert.assertEquals(expectedResult, answer);
+		String expectedResult = "220";
+		String answerReceived = cal.calculate(expression);
+		Assert.assertEquals(expectedResult, answerReceived);
 	}
 	
 	@Test
 	public void SubtractWithMultipleDigitWidthTest() {
 		Calculator cal = new Calculator();
 		String expression = "22-20";
-		String answer = "2";
-		String expectedResult = cal.calculate(expression);
-		Assert.assertEquals(expectedResult, answer);
+		String expectedResult = "2";
+		String answerReceived = cal.calculate(expression);
+		Assert.assertEquals(expectedResult, answerReceived);
 	}
 	
 	@Test
 	public void AddWithMultipleDigitWidthTest() {
 		Calculator cal = new Calculator();
 		String expression = "435+56";
-		String answer = "491";
-		String expectedResult = cal.calculate(expression);
-		Assert.assertEquals(expectedResult, answer);
+		String expectedResult = "491";
+		String answerReceived = cal.calculate(expression);
+		Assert.assertEquals(expectedResult, answerReceived);
 	}
 	
 	@Test
 	public void operatorBeforeNumberTest() {
 		Calculator cal = new Calculator();
 		String expression = "+4+5";
-		String answer = "ERROR: Inavlid String Input";
-		String expectedResult = cal.calculate(expression);
-		Assert.assertEquals(expectedResult, answer);
+		String expectedResult = "ERROR: Inavlid String Input";
+		String answerReceived = cal.calculate(expression);
+		Assert.assertEquals(expectedResult, answerReceived);
 	}
 	
 	@Test
 	public void onlyDigitInputTest() {
 		Calculator cal = new Calculator();
 		String expression = "5";
-		String answer = "5";
-		String expectedResult = cal.calculate(expression);
-		Assert.assertEquals(expectedResult, answer);
+		String expectedResult = "5";
+		String answerReceived = cal.calculate(expression);
+		Assert.assertEquals(expectedResult, answerReceived);
 	}
 
 	@Test
 	public void LongInputTest() {
 		Calculator cal = new Calculator();
 		String expression = "1+2*3+4*5-20+1";
-		String answer = "8";
-		String expectedResult = cal.calculate(expression);
-		Assert.assertEquals(expectedResult, answer);
+		String expectedResult = "8";
+		String answerReceived = cal.calculate(expression);
+		Assert.assertEquals(expectedResult, answerReceived);
 	}
 	
 	@Test
 	public void NonDigitOrOperatorInputTest() {
 		Calculator cal = new Calculator();
 		String expression = "abc";
-		String answer = "ERROR: Invalid String Input";
-		String expectedResult = cal.calculate(expression);
-		Assert.assertEquals(expectedResult, answer);
+		String expectedResult = "ERROR: Invalid String Input";
+		String answerReceived = cal.calculate(expression);
+		Assert.assertEquals(expectedResult, answerReceived);
 	}
 }
